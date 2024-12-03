055cf031d9ec3bf6844b946230c93ab1244e1a9f
Priyanshi1908 <115202982+Priyanshi1908@users.noreply.github.com>
2024-12-03 22:37:53 +0530

    Changed render.build.sh

--- render-build.sh
+++ render-build.sh
@@ -8,15 +8,20 @@
 
    echo "PortAudio installed"
 
-   # Verify installation
-   dpkg -l | grep portaudio
-
    # Upgrade pip
    echo "Upgrading pip"
    pip install --upgrade pip
 
+   # Create and activate a virtual environment
+   python3 -m venv venv
+   source venv/bin/activate
+
    # Install Python dependencies from requirements.txt
    echo "Installing Python dependencies"
    pip install -r requirements.txt
+
+   # Verify gunicorn installation
+   echo "Gunicorn location:"
+   which gunicorn
 
    echo "Script execution completed"
